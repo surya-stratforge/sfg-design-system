9c47cff9e5b3244683a53cda29ee23f7bf4a6554
surya-stratforge <surya.thangavel@stratforge.com>
2025-06-30 15:16:40 +0530

    chore: types updates

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -1,23 +1,23 @@
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import postcss from 'rollup-plugin-postcss';
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
 
 export default {
-  input: 'index.ts',
+  input: "index.ts",
   output: [
     {
-      dir: 'dist',
-      format: 'esm',
+      dir: "dist",
+      format: "esm",
       sourcemap: true,
-      preserveModules: false,
+      preserveModules: true,
     },
   ],
-  external: ['react', 'react-dom'],
+  external: ["react", "react-dom"],
   plugins: [
     resolve(),
     commonjs(),
-    typescript({ tsconfig: './tsconfig.json' }),
+    typescript({ tsconfig: "./tsconfig.json" }),
     postcss({
       extract: true,
       modules: false,
