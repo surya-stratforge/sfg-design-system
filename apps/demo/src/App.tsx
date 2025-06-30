0caa2cd2c8d091c3bf4f62697f6fd8baa3cd949a
surya-stratforge <surya.thangavel@stratforge.com>
2025-06-30 15:36:37 +0530

    chore: module type export updated

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,8 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { Button } from '@sfg/ui';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@sfg/ui";
+import type { ButtonSize } from "@sfg/ui/types";
+import "@sfg/ui/styles/index.css";
+
+export type TempType = ButtonSize;
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,12 +23,16 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
