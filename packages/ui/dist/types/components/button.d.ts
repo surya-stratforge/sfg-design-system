f2c550dcaecc724913d5a124fa30c5f2798a7209
surya-stratforge <surya.thangavel@stratforge.com>
2025-06-30 15:04:30 +0530

    chore: type declarations build updates

--- packages/ui/dist/types/components/button.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/index.css';
-type ButtonProps = {
-    children: React.ReactNode;
-    onClick?: () => void;
-};
-export declare const Button: React.FC<ButtonProps>;
-export {};
